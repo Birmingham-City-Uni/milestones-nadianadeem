a68208906edadfa856d9cd94c4399c37376e63d1
Nadia <nadianadeem365@gmail.com>
2020-10-28 18:26:31 +0000

    V2 - Created Screen
    Need to create README ASAP!

--- finalProject/FinalProject/FinalProject/GameLoop.cpp
+++ finalProject/FinalProject/FinalProject/GameLoop.cpp
@@ -9,6 +9,29 @@ GameLoop::GameLoop()
 
 bool GameLoop::init()
 {
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		std::cerr << "Could not initialise SDL: " << SDL_GetError();
+		return false;
+	}
+	window = SDL_CreateWindow(
+		"Space shooter",
+		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+		1280, 768,
+		SDL_WINDOW_SHOWN
+	);
+
+	if (!window) {
+		std::cerr << "Could not create SDL window: " << SDL_GetError();
+		return false;
+	}
+
+	// Create renderer
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	if (!renderer) {
+		std::cout << "Error creating renderer:" << SDL_GetError() << std::endl;
+		return false;
+	}
+
 	return true;
 }
 
--- finalProject/FinalProject/FinalProject/main.cpp
+++ finalProject/FinalProject/FinalProject/main.cpp
@@ -4,6 +4,13 @@
 
 GameLoop* gameLoop;
 
-int main(int argc, char* args[]) {
+int main(int argc, char* args[]) 
+{
+	gameLoop = new GameLoop();
+
+	if (!gameLoop->init()) {
+		return 1;
+	}
+
 	return 0;
 }
