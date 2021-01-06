ea9be4825599be052730b0cbbcd9c45f94521d60
nadianadeem <55580386+nadianadeem@users.noreply.github.com>
2021-01-06 15:46:19 +0000

    Additional comments added to bullet.h

--- finalProject/FinalProject/FinalProject/Bullets.h
+++ finalProject/FinalProject/FinalProject/Bullets.h
@@ -1,5 +1,7 @@
 #pragma once
 
+//Simple structs for the bullets. Stores their x, y rotation angle and distance travelled.
+//This data is used to draw the bullets correctly to the window and delete them accordingly.
 struct Bullets {
 	float x, y, rotation, distance;
 };
