c603ebd4a77bc7bae7f07b295107c97178e524b5
nadianadeem <55580386+nadianadeem@users.noreply.github.com>
2021-01-06 16:00:48 +0000

    Additional Comments added to GameLoop.cpp

--- finalProject/FinalProject/FinalProject/GameLoop.cpp
+++ finalProject/FinalProject/FinalProject/GameLoop.cpp
@@ -14,7 +14,7 @@ GameLoop::GameLoop()
 	textRenderer = nullptr;
 	sp = nullptr;
 
-	//
+	//Stores user input.
 	for (int i = 0; i < 512; i++) {
 		keyDown[i] = false;
 	}
@@ -22,6 +22,7 @@ GameLoop::GameLoop()
 
 bool GameLoop::init()
 {
+	//Trys to initialise SDL, if not an error message appears.
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
 		std::cerr << "Could not initialise SDL: " << SDL_GetError();
 		return false;
@@ -35,12 +36,13 @@ bool GameLoop::init()
 		SDL_WINDOW_SHOWN
 	);
 
+	//If a window cannot be created an error message is output to the console window.
 	if (!window) {
 		std::cerr << "Could not create SDL window: " << SDL_GetError();
 		return false;
 	}
 
-	// Create renderer
+	// Create renderer. If a renderer cannot be created an error message is displayed in the console window.
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 	if (!renderer) {
 		std::cout << "Error creating renderer:" << SDL_GetError() << std::endl;
@@ -59,33 +61,41 @@ bool GameLoop::init()
 	tm = new TiledMap(this->renderer);
 	tm->init();
 
+	//Initialises the sound player and plays the backing track to the game.
 	sp = new SoundPlayer();
 	sp->PlaySound(Sounds::BACKINGTRACK);
 
 
+	//Initialises the player with the renderer as a parameter so the player can be drawn.
+	//The tilemap is also a parameter to monitor the player's location on the map, as well as controlling collisions with the tilemap. 
 	player = new Player(this->renderer, this->tm);
 	player->init();
 
 	player->tiledMap = tm;
 
+	//Initialises the bullet manager with the renderer as a parameter so the bullets can be drawn to the window.
+	//The player is another parameter so the bullets can be drawn and moved in the correct position and direction.
+	//The sound player is a parameter so a shooting sound can be played when a bullet is shot.
 	bm = new BulletManager(this->renderer, this->player, this->sp);
 	bm->init();
 
+	//Initialises the zombie spawner with the renderer as a parameter so the zombies can be drawn to the window.
+	//The bullet manager is a parameter to track collisions between the bullets and the zombies.
+	//The player is a parameter to track collsions between the player and the zombies.
+	//The tilemap is a parameter to track the locations of zombies and is used in the pathfinding algorithm to get a path from the zombie to the player.
 	zs = new ZombieSpawner(this->renderer, this->bm, this->player, this->tm);
 	zs->init();
 
-
+	//Textrenderer is initialised with the arial font at a size of 30.
 	textRenderer = new TextRenderer("arial.ttf", 30);
 
 	return true;
 }
 
+//Process input method is used to get the SDL_Event and use it to find which keys have been pressed and released.
 bool GameLoop::processInput() 
 {
 	SDL_Event e;
-		
-	//process any input for game classes here
-	//player->processInput(e);
 
 	while (SDL_PollEvent(&e)) {
 		if (e.type == SDL_QUIT) {
@@ -106,12 +116,14 @@ bool GameLoop::processInput()
 
 	}
 
+	//Process input methods are carried out after the input has been processed.
 	bm->processInput(keyDown);
 	player->processInput(keyDown);
 
 	return true;
 }
 
+//Objects update methods are ran here.
 void GameLoop::update() 
 {
 	bm->update();
@@ -119,6 +131,7 @@ void GameLoop::update()
 	player->update();
 }
 
+//Objects draw methods are ran here as well as using the zombieString and the getZombiesHit method to renderer the players score.
 void GameLoop::draw() 
 {
 	//Draws everything to the window.
@@ -140,6 +153,7 @@ void GameLoop::draw()
 	SDL_Delay(16);
 }
 
+//Frees memory after the game has been played by destroying any textures.
 void GameLoop::clean() 
 {
 	bg->clean();
