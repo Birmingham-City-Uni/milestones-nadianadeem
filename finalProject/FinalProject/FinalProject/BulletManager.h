a20cbd8e464ef0fb10d9b6b040de601eea793857
nadianadeem <55580386+nadianadeem@users.noreply.github.com>
2021-01-06 15:44:19 +0000

    Additional comments added to bulletManager.h

--- finalProject/FinalProject/FinalProject/BulletManager.h
+++ finalProject/FinalProject/FinalProject/BulletManager.h
@@ -16,6 +16,7 @@ using namespace std;
 
 class BulletManager {
 	friend class ZombieSpawner;
+//Creates methods for the Bullet Manager to use.
 public:
 	BulletManager(SDL_Renderer* renderer, Player* player, SoundPlayer* sp);
 	void init();
@@ -25,14 +26,21 @@ public:
 	void clean();
 
 private:
+//Stores the renderer and texture to be drawn to the screen.
 	SDL_Renderer* renderer;
 	SDL_Texture* bulletTexture;
+//Stores all the bullets that are created by the user that need to be presented on the screen.
 	vector<Bullets>bullets;
+//Stores the player and sound player so their methods and variables can be manipulated by the bullet manager script.
 	Player* player;
 	SoundPlayer* sp;
-
+	
+//Center is the point where the bullet will be shot from.
 	SDL_Point center = { 1, 1 };
+//Integer that is used to stop the user spamming the spacebar and shooting lots of bullets.
 	const int SHOOT_DELAY = 300;
+//How quick the bullets will travel;
 	const int BULLET_VELOCITY = 15;
+//Stores when the last bullet was shot.
 	unsigned int lastShot = 0;
 };
