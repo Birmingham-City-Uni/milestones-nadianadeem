e832a038a8185fc388e5bed8efd0112fdfc468f4
nadianadeem <55580386+nadianadeem@users.noreply.github.com>
2021-01-06 16:20:09 +0000

    Additional comments to player.h

--- finalProject/FinalProject/FinalProject/Player.h
+++ finalProject/FinalProject/FinalProject/Player.h
@@ -9,6 +9,7 @@ using namespace std;
 
 class Player {
 public:
+//Methods created for the player object.
 	Player(SDL_Renderer*, TiledMap*);
 	void init();
 	void exit();
@@ -18,35 +19,46 @@ public:
 	void clean();
 	float getPosX();
 	float getPosY();
-
+	
+//These 4 booleans are used to track which way the player is facing.
 	bool facingRight;
 	bool facingLeft;
 	bool facingUp;
 	bool facingDown;
 
+//Stores a tilemap to be used within methods.
 	TiledMap* tiledMap;
 
+//Creates 2 integers to keep track of the player's walkable position.
 	int oldX = position.x;
 	int oldY = position.y;
 
+//Stores an SDL_Rect position to store the player's position.
 	SDL_Rect position;
 
+//Creates a float to store the players health.
 	float health = 100;
+//Stores the player's location as a tile number.
 	int playerLoc;
 
+//Stores the players X and Y in the context of the tile map.
 	int playerTileX;
 	int playerTileY;
 
+//This boolean is used to for the unique game mechanic which is the bomb pick up.
 	bool removeZombies = false;
 
+//These SDL_Rects are used to check tile map collisions.
 	SDL_Rect playerPos;
 	SDL_Rect mapTile;
 
 private:
-
+//Stores the renderer and texture so the player can be drawn to the screen.
 	SDL_Renderer* renderer;
 	SDL_Texture* texture;
+//Stores the angle at which the player is at.
 	int angle = 0;
+//Stores whether the image of the player has been flipped.
 	SDL_RendererFlip flip = SDL_FLIP_NONE;
 	
 };
